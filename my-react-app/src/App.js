49c3b29128a0cc0a021f92be583f8ab6afa87f0b
fenyuchenabc <fenyuchenabc@hotmail.com>
2024-10-21 13:00:06 -0400

    install react framework

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>Welcome to my first React app.</p>
+    </div>
+  );
+}
+
+
+export default App;
