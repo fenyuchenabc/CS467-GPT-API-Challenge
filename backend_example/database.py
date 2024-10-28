7b309bb23727c2adce5d058c276c7026d946c879
Khrystian Clark <clarkkhr@engr.orst.edu>
2024-10-28 14:48:24 -0700

    Added error checking to backend functions in order to help with future code additions and debugging. Also closes database in there case that the save_story function errors out.

--- backend_example/database.py
+++ backend_example/database.py
@@ -3,28 +3,37 @@ import sqlite3
 
 class StoryDatabase:
     def __init__(self):
-        self.sqlconn = sqlite3.connect('story_data.db')
-        self.create_table()
+        try:
+            self.sqlconn = sqlite3.connect('story_data.db')
+            self.create_table()
+        except sqlite3.Error as e:
+            print(f"Error connecting to database: {e}")
 
     def create_table(self):
-        query = '''CREATE TABLE IF NOT EXISTS story_data (
-            story_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            genre VARCHAR(60) NOT NULL,
-            age INTEGER NOT NULL,
-            choice_count INTEGER NOT NULL,
-            segment_count INTEGER NOT NULL,
-            content TEXT NOT NULL
-        )'''
-
-        self.sqlconn.execute(query)
-        self.sqlconn.commit()
+        try:
+            query = '''CREATE TABLE IF NOT EXISTS story_data (
+                story_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                genre VARCHAR(60) NOT NULL,
+                age INTEGER NOT NULL,
+                choice_count INTEGER NOT NULL,
+                segment_count INTEGER NOT NULL,
+                content TEXT NOT NULL
+            )'''
+            self.sqlconn.execute(query)
+            self.sqlconn.commit()
+        except sqlite3.Error as e:
+            print(f"Error creating table: {e}")
 
     def save_story(self, genre, age, choice_count, segment_count, content):
-        query = '''INSERT INTO story_data (genre, age, choice_count, segment_count, content)
-                VALUES (?, ?, ?, ?, ?)'''
+        try:
+            query = '''INSERT INTO story_data (genre, age, choice_count, segment_count, content)
+            VALUES (?, ?, ?, ?, ?)'''
 
-        self.sqlconn.execute(query, (genre, age, choice_count, segment_count, content))
-        self.sqlconn.commit()
+            self.sqlconn.execute(query, (genre, age, choice_count, segment_count, content))
+            self.sqlconn.commit()
+        except sqlite3.Error as e:
+            print(f"Error saving story: {e}")
+            return None
 
     def close(self):
         self.sqlconn.close()
